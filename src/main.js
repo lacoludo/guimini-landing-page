743c09b374146f2ea3fee30620626363ba6328a7
lacoludo <email@lacoludo.com>
2019-10-28 14:06:27 +0100

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,21 @@
+// CSS libraries
+import 'bootstrap/dist/css/bootstrap.css'
+import './lib/animate.css/animate.min.css'
+import 'owl-carousel-2/assets/owl.carousel.css'
+import './lib/jquery.meanmenu/meanmenu.css'
+import './lib/icofont/css/icofont.min.css'
+import 'font-awesome/css/font-awesome.css'
+
+// Styles
+import './scss/responsive.scss'
+import './scss/main.scss'
+
+// JavaScript libraries
+import './lib/jquery.meanmenu/jquery.meanmenu.min'
+import 'owl-carousel-2/owl.carousel'
+import 'bootstrap-validator'
+
+// Scripts
+import './js/contact'
+import './js/map'
+import './js/main'
